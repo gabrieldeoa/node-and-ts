26681ab0f94d1d1293d20d61b1ff3a7cb4b91d1f
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-08-29 19:43:53 -0300

    Change column Id Type

--- src/database/migrations/1598494990828-CreateAppointments.ts
+++ src/database/migrations/1598494990828-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1598494990828
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
