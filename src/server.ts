b2468803bdab7a7441d8686cfdbfcb46abae69d2
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-09-01 21:13:47 -0300

    create static route to get files

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 import './database';
 
 const PORT = 3333;
@@ -8,6 +9,9 @@ const PORT = 3333;
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.listen(PORT, () => {
